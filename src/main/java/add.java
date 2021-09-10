a4259f043a7a2ff27ba94b382c6af15d4dc1f3e3
lavakush7800 <lavakushgiri@gmail.com>
2021-09-10 13:37:25 +0530

    first commit

---
+++ src/main/java/add.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number : ");
+		int a = sc.nextInt();
+		System.out.println("Enter Number : ");
+		int b = sc.nextInt();
+		int c=a+b;
+		System.out.print("Your Anser is : ");
+		System.out.println(c);
+
+	}
+
+}
