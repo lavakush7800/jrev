c0ee2df8e85333e023a0088d5f34a26cf6bb07fc
lavakush7800 <lavakushgiri@gmail.com>
2021-09-16 08:50:58 +0530

    pat1

--- src/main/java/Pattern1.java
+++ src/main/java/Pattern1.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 
 public class Pattern1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number You want to print : ");
+		int n = sc.nextInt();
+		for(int i=1; i<=n; i++) {
+			System.out.println(i+" ");
+		}
+		System.out.println();
 
 	}
 
