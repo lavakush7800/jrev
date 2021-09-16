f5b0fd627115a66a58e8d0920dd5de2e87b05aba
lavakush7800 <lavakushgiri@gmail.com>
2021-09-16 09:14:27 +0530

    patt3

--- src/main/java/Patt3.java
+++ src/main/java/Patt3.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
-public class Pat1 {
+public class Patt3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Number You want to print : ");
+		System.out.println("Enter Number you Want to print : ");
 		int n = sc.nextInt();
-		
 		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=n; j++) {
-				System.out.print(i+" ");
+			for(int j=n; j>=i; j--) {
+				System.out.print("*"+" ");
 			}
 			System.out.println();
 		}
