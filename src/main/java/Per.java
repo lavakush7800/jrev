4b759e4c1c4d4f3ce1aa3e12422cafb05cc0d145
lavakush7800 <lavakushgiri@gmail.com>
2021-09-27 19:43:31 +0530

    per

--- src/main/java/Per.java
+++ src/main/java/Per.java
@@ -1,17 +1,9 @@
-import java.util.Scanner;
 
 public class Per {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Enter Physics Marks: ");
-		int n= sc.nextInt();
-		
-		if(n<=90 && n>=100) {
-			System.out.println();
-		}
+
 	}
 
 }
