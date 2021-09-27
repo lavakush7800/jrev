2658ea066dd9a1cc8c91ebfa7f63bed6276258cc
lavakush7800 <lavakushgiri@gmail.com>
2021-09-27 19:37:41 +0530

    per

--- src/main/java/Per.java
+++ src/main/java/Per.java
@@ -5,6 +5,7 @@ public class Per {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Physics Marks: ");
 		int n = sc.nextInt();
 
 	}
