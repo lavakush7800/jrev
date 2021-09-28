d31bcf723a0ff4cbf78b76a79446af25091e4438
lavakush7800 <lavakushgiri@gmail.com>
2021-09-28 19:07:56 +0530

    Per

--- src/main/java/Percentage.java
+++ src/main/java/Percentage.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 
 public class Percentage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
 
 	}
 
