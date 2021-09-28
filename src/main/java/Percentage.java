ca5a2e9fab40ebf2472c1e3be791970fb6c5ddd1
lavakush7800 <lavakushgiri@gmail.com>
2021-09-28 19:13:29 +0530

    Per

--- src/main/java/Percentage.java
+++ src/main/java/Percentage.java
@@ -6,20 +6,20 @@ public class Percentage {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println();
+		System.out.print("Enter Number of Maths :");
 		int math = sc.nextInt();
 		
-		System.out.println();
+		System.out.print("Enter Number of Chemistry :");
 		int chemistry = sc.nextInt();
 		
-		System.out.println();
+		System.out.print("Enter Number of Physics :");
 		int physics = sc.nextInt();
 		
-		System.out.println();
+		System.out.print("Enter Number of Hindi :");
 		int hindi = sc.nextInt();
 		
-		System.out.println();
-		int engilsh = sc.nextInt();
+		System.out.print("Enter Number of English :");
+		int english = sc.nextInt();
 
 	}
 
