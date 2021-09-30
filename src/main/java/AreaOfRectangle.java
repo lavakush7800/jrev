4d1da2e8b367111506e02edd4eb7d8a7bec20d0f
lavakush7800 <lavakushgiri@gmail.com>
2021-09-30 20:00:50 +0530

    Percentage

--- src/main/java/AreaOfRectangle.java
+++ src/main/java/AreaOfRectangle.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 
 public class AreaOfRectangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println();
+		float hieght = sc.nextFloat();
+		
 
 	}
 
