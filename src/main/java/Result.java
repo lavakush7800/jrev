fa3ab29e9148328319bb2877836fbd7752ace5b8
lavakush7800 <lavakushgiri@gmail.com>
2021-09-30 15:29:28 +0530

    test

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 
 public class Result {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println();
+		int hindi = sc.nextInt();
 
 	}
 
