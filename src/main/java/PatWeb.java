9c1dd93ada475f88cdea0204fdd1da71df38614b
lavakush7800 <lavakushgiri@gmail.com>
2021-10-02 22:16:53 +0530

    pat web

--- src/main/java/PatWeb.java
+++ src/main/java/PatWeb.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 
 public class PatWeb {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
 	}
 
