1bceed9f5e40a4a66604e9c0bc45ec4e0e9bac4e
lavakush7800 <lavakushgiri@gmail.com>
2021-10-02 22:31:18 +0530

    pat web

--- src/main/java/PatWeb.java
+++ src/main/java/PatWeb.java
@@ -8,7 +8,7 @@ public class PatWeb {
 		System.out.println("Enter the number : ");
 		int n = sc.nextInt();
 		
-		for(int i=1;i<=n;i++) {
+		for(int i=n;i<=1;i--) {
 			for(int j=1; j<=i;j++) {
 				System.out.print("*"+" ");
 			}
