aa8d90fd0362addf9839015c4393696b7103b5b1
lavakush7800 <lavakushgiri@gmail.com>
2021-10-02 22:28:05 +0530

    pat web

--- src/main/java/PatWeb.java
+++ src/main/java/PatWeb.java
@@ -9,7 +9,10 @@ public class PatWeb {
 		int n = sc.nextInt();
 		
 		for(int i=1;i<=n;i++) {
-				System.out.println(i);
+			for(int j=1; j<=i;j++) {
+				System.out.print(i+" ");
+			}
+				System.out.println();
 		}
 
 	}
