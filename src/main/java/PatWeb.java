77273744c50274eb6426ccfec6d751dd3df69705
lavakush7800 <lavakushgiri@gmail.com>
2021-10-02 22:17:54 +0530

    pat web

--- src/main/java/PatWeb.java
+++ src/main/java/PatWeb.java
@@ -5,6 +5,7 @@ public class PatWeb {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number : ");
 
 	}
 
