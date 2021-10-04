7256e0318d174542585ad5c0f45da5adf9fd348c
lavakush7800 <lavakushgiri@gmail.com>
2021-10-04 19:25:26 +0530

    erb pat

--- src/main/java/PatWeb.java
+++ src/main/java/PatWeb.java
@@ -8,13 +8,7 @@ public class PatWeb {
 		System.out.println("Enter the number : ");
 		int n = sc.nextInt();
 		
-		for(int i=1;i<=n;i++) {
-			for(int j=1; j<=i;j++) {
-				System.out.print("*"+" ");
-			}
-				System.out.println();
-		}
-
+		
 	}
 
 }
