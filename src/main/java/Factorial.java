7f5f2d2c739783b7061b183bfb77e3ded78e52cf
lavakush7800 <lavakushgiri@gmail.com>
2021-10-05 19:38:30 +0530

    Factorial

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 
 public class Factorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
 
 	}
 
