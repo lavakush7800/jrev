f9b2a678384cb947051260c689d4efef3b3c8840
lavakush7800 <lavakushgiri@gmail.com>
2021-10-05 19:47:55 +0530

    Factorial

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -13,8 +13,6 @@ public class Factorial {
 			for(int i=n; i>=1; i--) 
 				fact = fact*i;
 			System.out.println("Factorial number "+n+" is "+fact);
-		}else {
-			System.out.println("You are Entered Wrong Number ");
 		}
 	}
 
