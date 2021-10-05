8ee905a09024ca5a3d97e9ddffe97851a90f4c88
lavakush7800 <lavakushgiri@gmail.com>
2021-10-05 19:07:50 +0530

    prime

--- src/main/java/PatWeb.java
+++ src/main/java/PatWeb.java
@@ -7,8 +7,11 @@ public class PatWeb {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number : ");
 		int n = sc.nextInt();
-		
-		
+		for(int i=1;i<=n;i++) {
+			if(i%n==0) {
+				System.out.println(i);
+			}
+		}
 	}
 
 }
