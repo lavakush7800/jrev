c43f58655c54387e3d2f85243fb6eeaaf3832f28
lavakush7800 <lavakushgiri@gmail.com>
2021-10-05 19:39:55 +0530

    Factorial

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -5,6 +5,7 @@ public class Factorial {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Factorial Number : ");
 		int n = sc.nextInt();
 
 	}
